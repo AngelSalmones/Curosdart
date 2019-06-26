60f81926302c0b8664ddd2c11ff40c1b257621da
TheDarkFish <angel_salmones@hotmail.com>
2019-06-26 17:02:08 -0500

    se realizo un ejercicio de tipos de numeros y de strings

---
+++ tiposnumeros.dart
@@ -0,0 +1,20 @@
+void main() {
+  
+//  tipos de Numeros
+  int empleados = 10;
+  double pi = 3.141592;
+  var numero = 1.0;
+  
+  print( '$empleados - $pi - $numero' );
+  
+//   String - Cadenas de caracteres
+  String nombre = "Angel";
+  for(int i=0;i<nombre.length;i++){
+    print (nombre[i]);
+    
+  }
+  for(int i=nombre.length-1 ;i>=0;i--){
+    print (nombre[i]);
+  }
+  
+}
