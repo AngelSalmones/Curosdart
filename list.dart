3e10b87c07a91e18442ececc47305441929bac56
AngelSalmones <angel.garcia@cceo.com.mx>
2019-06-27 11:54:37 -0500

    program with list

---
+++ list.dart
@@ -0,0 +1,17 @@
+void main (){
+  //new list with friends
+  List <String> friends = ["carlos","pedro","juan","pepe"];
+  
+  print (friends);
+  print (friends[2]);
+  friends.add("angel");
+  print (friends);
+  //static list
+  List numbers = List (10);
+  print (numbers);
+  numbers[4]=10;
+  print (numbers);
+  
+  
+  
+}
