0e7d5329603a11bdf564e92c3830a3e342f73512
AngelSalmones <angel.garcia@cceo.com.mx>
2019-06-27 11:20:10 -0500

    exercise of number's type and strings

---
+++ numbers.dart
@@ -0,0 +1,19 @@
+void main (){
+  
+  //types of numbers
+  
+  int apples = 2;
+  double pi = 3.1415;
+  // but double everytime whit point
+  double people = 2.0;
+  
+  print ("$apples + $pi + $people");
+  
+  String name = "Angel";
+  
+  for (int i = 0;i<name.length; i++){
+    print (name[i]);
+  }
+ 
+  
+}
